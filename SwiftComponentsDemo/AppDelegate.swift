852c8a7286819e7a15c4b6af63ae6606cc07448f
小梁同学 <350442340@QQ.com>
2020-07-22 08:28:29 +0800

    update

---
+++ SwiftComponentsDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftComponentsDemo
+//
+//  Created by liangze on 2020/7/22.
+//  Copyright © 2020 liangze. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+
+}
+
---
+++ SwiftComponentsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftComponentsDemo
+//
+//  Created by liangze on 2020/7/22.
+//  Copyright © 2020 liangze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
